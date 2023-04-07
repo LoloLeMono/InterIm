8ad6d261c3a6e29f387a089bcf5d7fd15df691ee
LoloLeMono <rouquairolr@gmail.com>
2023-04-07 12:44:50 +0200

    Fix home fragment position

--- app/src/main/java/com/mobile/inter_im/ui/home/HomeViewModel.java
+++ app/src/main/java/com/mobile/inter_im/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Découvrez votre futur job !");
     }
 
     public LiveData<String> getText() {
