5eae226e8ee08f5d679f55a52b2ed60de7658cc2
LoloLeMono <rouquairolr@gmail.com>
2023-04-08 15:47:50 +0200

    Add rapid search

--- app/src/main/java/com/mobile/inter_im/ui/home/HomeFragment.java
+++ app/src/main/java/com/mobile/inter_im/ui/home/HomeFragment.java
@@ -22,7 +22,7 @@ private FragmentHomeBinding binding;
     binding = FragmentHomeBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
+        final TextView textView = binding.textHomeTitle;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
