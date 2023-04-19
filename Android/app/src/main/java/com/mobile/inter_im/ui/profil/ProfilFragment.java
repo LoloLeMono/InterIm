934c67321686d5ba840aacdf2efcad5c9cf84f86
LoloLeMono <rouquairolr@gmail.com>
2023-04-19 19:20:40 +0200

    Add login form

--- Android/app/src/main/java/com/mobile/inter_im/ui/profil/ProfilFragment.java
+++ Android/app/src/main/java/com/mobile/inter_im/ui/profil/ProfilFragment.java
@@ -24,8 +24,8 @@ private FragmentProfilBinding binding;
     binding = FragmentProfilBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
 
-        final TextView textView = binding.textProfil;
-        profilViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        /*final TextView textView = binding.textProfil;
+        profilViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);*/
         return root;
     }
 
